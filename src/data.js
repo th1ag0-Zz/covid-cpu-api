0def8a11e8666c4e43e8e7d9375d5584efd95385
Thiago <thiagoF89.tf@gmail.com>
2021-03-18 22:57:44 -0300

    :tada: estrutura de json atualizada

--- src/data.js
+++ src/data.js
@@ -1,13 +1,18 @@
 const data = {
-  "confirmados": "959",
-  "recuperados": "904",
-  "ativos": "34",
-  "obitos": "21",
-  "descartados": "3541",
-  "suspeitos": "332",
-  "atualizacao": {
-    "data": "16/03/2021",
-    "hora": "20:26"
+  covid: {
+    "confirmados": "959",
+    "recuperados": "904",
+    "ativos": "34",
+    "obitos": "21",
+    "descartados": "3541",
+    "suspeitos": "332",
+    "atualizacao": {
+      "data": "16/03/2021",
+      "hora": "20:26"
+    }
+  },
+  vacina: {
+
   }
 }
 
