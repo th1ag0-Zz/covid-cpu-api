f5b2211bf540c6e4e7ad12e4f73b12fed955d0ea
Thiago <thiagoF89.tf@gmail.com>
2021-03-19 00:47:12 -0300

    :tada: atualização 18/03

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 const data = {
   covid: {
-    "confirmados": "959",
-    "recuperados": "904",
-    "ativos": "34",
+    "confirmados": "1002",
+    "recuperados": "906",
+    "ativos": "75",
     "obitos": "21",
-    "descartados": "3541",
-    "suspeitos": "332",
+    "descartados": "3599",
+    "suspeitos": "275",
     "atualizacao": {
-      "data": "16/03/2021",
-      "hora": "20:26"
+      "data": "18/03/2021",
+      "hora": "20:06"
     }
   },
   vacina: {
