57a439d7c724c5573cba5f5b5b1b5aa13a0b9a6a
Thiago <thiagoF89.tf@gmail.com>
2021-03-23 18:27:43 -0300

    :tada: add politica

---
+++ pages/privacy_policy.js
@@ -0,0 +1,57 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Termos e condições gerais de uso do aplicativo COVID CPU</h1>
+
+      <p>
+      Os serviços do COVID CPU são fornecidos pela pessoa física com o seguinte nome: Lídia Raquel Almeida de Abreu, inscrito no CPF sob o nº 952.232.153-20 e Thiago Furtado Silva, inscrito no CPF sob o nº 077.825.483-65, titulares da propriedade intelectual sobre o aplicativo COVID CPU e seus respectivos conteúdos e demais ativos, instalados na plataforma Android.
+1. Do objeto
+O aplicativo, e demais ativos de propriedade intelectual, fornece ferramentas para informar, auxiliar e dinamizar o dia a dia dos seus usuários.
+Caracteriza-se, ainda, pela prestação do seguinte serviço: divulgação e compilação de informações sobre a doença COVID 19, ocasionada pelo vírus Sars-cov2, no município de Cururupu/MA.
+2. Da aceitação
+O presente Termo estabelece obrigações contratadas de livre e espontânea vontade, por tempo indeterminado, entre o aplicativo da plataforma android e as pessoas físicas ou jurídicas, usuárias do aplicativo.
+Ao utilizar a plataforma o usuário aceita integralmente as presentes normas e compromete-se a observá-las, sob o risco de aplicação das penalidades cabíveis.
+A aceitação do presente instrumento é imprescindível para o acesso e para a utilização de quaisquer serviços fornecidos. Caso não concorde com as disposições deste instrumento, o usuário não deve utilizá-los.
+3. Do acesso dos usuários
+Serão utilizadas todas as soluções técnicas à disposição do responsável pela plataforma para permitir o acesso ao serviço 24 (vinte e quatro) horas por dia, 7 (sete) dias por semana. No entanto, a navegação na plataforma ou em alguma de suas páginas poderá ser interrompida, limitada ou suspensa para atualizações, modificações ou qualquer ação necessária ao seu bom funcionamento.
+4. Do suporte
+Em caso de qualquer dúvida, sugestão ou problema com a utilização da plataforma, o usuário poderá entrar em contato com o suporte, através do email: lidiaraquel11@gmail.com, telefone 98 9 81794372 e thiagof89.tf@gmail.com, telefone (98) 984500045.
+Estes serviços de atendimento ao usuário estarão disponíveis nos horários comerciais.
+05. Das responsabilidades
+É de responsabilidade do usuário:
+a) defeitos ou vícios técnicos originados no próprio sistema do usuário;
+b) a correta utilização da plataforma, dos serviços ou produtos oferecidos, prezando pela boa convivência, pelo respeito e cordialidade entre os usuários;
+c) pelo cumprimento e respeito ao conjunto de regras disposto nesse Termo de Condições Geral de Uso, na respectiva Política de Privacidade e na legislação nacional e internacional;
+d) pela proteção aos dados de acesso à sua conta/perfil (login e senha).
+É de responsabilidade do aplicativo COVID CPU:
+a) indicar as características do serviço ou produto;
+b) os defeitos e vícios encontrados no serviço ou produto oferecido desde que lhe tenha dado causa;
+c) as informações que foram por ele divulgadas, sendo que os comentários ou informações divulgadas por usuários são de inteira responsabilidade dos próprios usuários;
+d) os conteúdos ou atividades ilícitas praticadas através do aplicativo.
+O aplicativo COVID CPU não se responsabiliza por links externos contidos em seu sistema que possam redirecionar o usuário à ambiente externo a sua rede.
+Não poderão ser incluídos links externos ou páginas que sirvam para fins comerciais ou publicitários ou quaisquer informações ilícitas, violentas, polêmicas, pornográficas, xenofóbicas, discriminatórias ou ofensivas.
+06. Dos direitos autorais
+O presente Termo de Uso concede aos usuários uma licença não exclusiva, não transferível e não sublicenciável, para acessar e fazer uso da plataforma e dos serviços e produtos por ela disponibilizados.
+A estrutura do site ou aplicativo, as marcas, logotipos, nomes comerciais, layouts, gráficos e design de interface, imagens, ilustrações, fotografias, apresentações, vídeos, conteúdos escritos e de som e áudio, programas de computador, banco de dados, arquivos de transmissão e quaisquer outras informações e direitos de propriedade intelectual das pessoas físicas Lídia Raquel Almeida de Abreu, inscrito no CPF sob o nº 952.232.153-20 e Thiago Furtado Silva, inscrito no CPF sob o nº 077.825.483-65, observados os termos da Lei da Propriedade Industrial (Lei nº 9.279/96), Lei de Direitos Autorais (Lei nº 9.610/98) e Lei do Software (Lei nº 9.609/98), estão devidamente reservados.
+Este Termos de Uso não cede ou transfere ao usuário qualquer direito, de modo que o acesso não gera qualquer direito de propriedade intelectual ao usuário, exceto pela licença limitada ora concedida.
+O uso do aplicativo pelo usuário é pessoal, individual e intransferível, sendo vedado qualquer uso não autorizado, comercial ou não-comercial. Tais usos consistirão em violação dos direitos de propriedade intelectual das pessoas físicas Lídia Raquel Almeida de Abreu, inscrito no CPF sob o nº 952.232.153-20 e Thiago Furtado Silva, inscrito no CPF sob o nº 077.825.483-65, puníveis nos termos da legislação aplicável.
+07. Das sanções
+Sem prejuízo das demais medidas legais cabíveis, das pessoas físicas Lídia Raquel Almeida de Abreu, inscrito no CPF sob o nº 952.232.153-20 e Thiago Furtado Silva, inscrito no CPF sob o nº 077.825.483-65 poderão, a qualquer momento, advertir, suspender ou cancelar a conta do usuário:
+a) que violar qualquer dispositivo do presente Termo;
+b) que descumprir os seus deveres de usuário;
+c) que tiver qualquer comportamento fraudulento, doloso ou que ofenda a terceiros.
+08. Da rescisão
+A não observância das obrigações pactuadas neste Termo de Uso ou da legislação aplicável poderá, sem prévio aviso, ensejar a imediata rescisão unilateral por parte da razão social COVID CPU e o bloqueio de todos os serviços prestados ao usuário.
+09. Das alterações
+Os itens descritos no presente instrumento poderão sofrer alterações, unilateralmente e a qualquer tempo, por parte das pessoas físicas Lídia Raquel Almeida de Abreu, inscrito no CPF sob o nº 952.232.153-20 e Thiago Furtado Silva, inscrito no CPF sob o nº 077.825.483-65, para adequar ou modificar os serviços, bem como para atender novas exigências legais. As alterações serão veiculadas  pelo aplicativo COVID CPU e o usuário poderá optar por aceitar o novo conteúdo ou por cancelar o uso dos serviços, caso seja assinante de algum serviço.
+10. Da política de privacidade
+Além do presente Termo, o usuário deverá consentir com as disposições contidas na respectiva Política de Privacidade a ser apresentada a todos os interessados dentro da interface da plataforma.
+16. Do foro
+Para a solução de controvérsias decorrentes do presente instrumento será aplicado integralmente o Direito brasileiro.
+Os eventuais litígios deverão ser apresentados no foro da comarca de Cururupu/MA.
+      </p>
+
+
+    </div>
+  )
+}
